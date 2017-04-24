acef6c1026f4d8396d465353f3e0fe61318ed653
Unknown <mhm.sherpa@gmail.com>
2017-04-24 18:59:52 -0500

    ;saldkjf

--- createTable.php
+++ createTable.php
@@ -19,9 +19,7 @@ $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 $link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
 
 
-$sql = "INSERT INTO users (id, fname, lname, age)
-VALUES (1, 'James', 'Casimir', 20)";
-
+$sql = "";
 if (mysqli_query($link, $sql)) {
     echo "New record created successfully";
 } else {
