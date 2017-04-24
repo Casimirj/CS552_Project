2ae5c6430a5d66e88a2658cc36f5e951c655e8ef
Unknown <mhm.sherpa@gmail.com>
2017-04-24 17:08:28 -0500

    again

--- createTable.php
+++ createTable.php
@@ -29,5 +29,5 @@ echo "Success: A proper connection to MySQL was made! The my_db database is grea
 echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
 
 mysqli_close($link);
-
+//haha commenting bceause fo whatever reason it wont recognize I changed this file
     ?>
