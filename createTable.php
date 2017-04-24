b43144e2ccd2f404d46fc34176215187e771447c
Unknown <mhm.sherpa@gmail.com>
2017-04-24 17:20:58 -0500

    new php stuffs

--- createTable.php
+++ createTable.php
@@ -1,6 +1,6 @@
 <?php
 
-    $connectstr_dbhost = '';
+$connectstr_dbhost = '';
 $connectstr_dbname = '';
 $connectstr_dbusername = '';
 $connectstr_dbpassword = '';
@@ -18,16 +18,17 @@ $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 
 $link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
 
-if (!$link) {
-echo "Error: Unable to connect to MySQL." . PHP_EOL;
-echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-exit;
+
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
 }
-
-echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
-
 mysqli_close($link);
-//haha commenting bceause fo whatever reason it wont recognize I changed this file
-    ?>
+
+
+
+?>
