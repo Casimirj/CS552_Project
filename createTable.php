8a3edd2a6d5529bada0bebffa03fc260101614a8
Unknown <mhm.sherpa@gmail.com>
2017-04-25 09:43:57 -0500

    stackoverflow qquestion anser (hopefully works)

--- createTable.php
+++ createTable.php
@@ -1,43 +1,46 @@
-    <?php
+<?php
 
-    $connectstr_dbhost = '';
-    $connectstr_dbname = '';
-    $connectstr_dbusername = '';
-    $connectstr_dbpassword = '';
+$connectstr_dbhost = '';
+$connectstr_dbname = '';
+$connectstr_dbusername = '';
+$connectstr_dbpassword = '';
 
-    foreach ($_SERVER as $key => $value) {
-    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
-    continue;
+foreach ($_SERVER as $key => $value)
+{
+    if (strpos($key, "MYSQLCONNSTR_") !== 0)
+    {
+        continue;
     }
 
     $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
     $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
     $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
     $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-    }
 
-    //$link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
-    //mysqli_error($link);
+}
 
-    //$sql = "INSERT INTO `users` (id, fname, lname, age)
-    //VALUES (1, `James`, `Casimir`, 20)";
+//$link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
+//mysqli_error($link);
 
-    if($link=mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname)) {
-        $sql = "INSERT INTO `users` (`id`,`fname`,`lname`,`age`) VALUES (1, 'James', 'Casimir', 20)";
-        if (mysqli_query($link, $sql)) {
-            if (($aff_rows = mysqli_affected_rows($link)) > 0) {
-                echo "New record created successfully ($aff_rows)";
-            } else {
-                echo "Query Logic Error: $sql";
-            }
+//$sql = "INSERT INTO `users` (id, fname, lname, age)
+//VALUES (1, `James`, `Casimir`, 20)";
+
+if($link=mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname)) {
+    $sql = "INSERT INTO `users` (`id`,`fname`,`lname`,`age`) VALUES (1, 'James', 'Casimir', 20)";
+    if (mysqli_query($link, $sql)) {
+        if (($aff_rows = mysqli_affected_rows($link)) > 0) {
+            echo "New record created successfully ($aff_rows)";
         } else {
-            echo "Syntax Error: ", mysqli_error($link);
+            echo "Query Logic Error: $sql";
         }
-        mysqli_close($link);
+    } else {
+        echo "Syntax Error: ", mysqli_error($link);
     }
-    else{
-        echo "Connection Error: ", mysqli_connect_error();
-    }
+    mysqli_close($link);
+}
+else{
+    echo "Connection Error: ", mysqli_connect_error();
+}
 
 
-    ?>
+?>
