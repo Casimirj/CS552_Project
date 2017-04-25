7cb743efce9ea8f51777647d0d4e3a4c1c08ceae
Unknown <mhm.sherpa@gmail.com>
2017-04-25 14:13:13 -0500

    cleaned up inputs in create function

--- acmedb.php
+++ acmedb.php
@@ -29,11 +29,11 @@
         }
 
 
-        public function create_user($username, $pass){
+        public function create_user($usernameInput, $passInput){
             global $connectstr_dbhost, $connectstr_dbname, $connectstr_dbpassword, $connectstr_dbusername;
 
             if($link=mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname)) {
-                $sql = "INSERT INTO `users` (`id`,`username`,`password`) VALUES (1, $username, $pass)";
+                $sql = "INSERT INTO `users` (`id`,`username`,`password`) VALUES (1, $usernameInput, $passInput)";
                 if (mysqli_query($link, $sql)) {
                     if (($aff_rows = mysqli_affected_rows($link)) > 0) {
                         echo "New record created successfully ($aff_rows)";
