3a71b65ba62d85a26de86e8e273be45f5cf7e3db
Unknown <mhm.sherpa@gmail.com>
2017-04-25 21:35:17 -0500

    Added re-enter password validation

--- acmedbCreateUser.php
+++ acmedbCreateUser.php
@@ -1,7 +1,6 @@
 
 
 <?php
-echo "SUCCESS!lolnah";
     require("acmedb.php");
 
     $database = new Acmedb();
@@ -10,13 +9,18 @@ echo "SUCCESS!lolnah";
 
     $name = $_POST['usernameinput'];
     $password = $_POST['passinput'];
+    $passvalidate = $_POST['passvalidate'];
     $fname = $_POST['fnameinput'];
     $lname = $_POST['lnameinput'];
     $email = $_POST['emailinput'];
 
-    $database->create_user($name, $password, $fname, $lname, $email);
+    if(password === passvalidate){
+        $database->create_user($name, $password, $fname, $lname, $email);
+    }
+    else{
+        echo "didnt create\n";
+    }
 
-    echo "SUCCESS!";
 
 
 ?>
