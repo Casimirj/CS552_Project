a8e9484555b3185f81969d893c1becbb5fa01d15
Unknown <mhm.sherpa@gmail.com>
2017-04-26 09:44:43 -0500

    .

--- acmedb.php
+++ acmedb.php
@@ -1,63 +1,40 @@
-    <?php
+<?php
 
-    class Acmedb {
-        private $connectstr_dbhost = '';
-        private $connectstr_dbname = '';
-        private $connectstr_dbusername = '';
-        private $connectstr_dbpassword = '';
+class Acmedb {
+    private $connectstr_dbhost = '';
+    private $connectstr_dbname = '';
+    private $connectstr_dbusername = '';
+    private $connectstr_dbpassword = '';
 
-        public function __construct(){
+    public function __construct(){
 
-        }
+    }
 
-        public function connect(){
-            global $connectstr_dbhost, $connectstr_dbname, $connectstr_dbpassword, $connectstr_dbusername;
+    public function connect(){
+        global $connectstr_dbhost, $connectstr_dbname, $connectstr_dbpassword, $connectstr_dbusername;
 
-            foreach ($_SERVER as $key => $value)
+        foreach ($_SERVER as $key => $value)
+        {
+            if (strpos($key, "MYSQLCONNSTR_") !== 0)
             {
-                if (strpos($key, "MYSQLCONNSTR_") !== 0)
-                {
-                    continue;
-                }
-
-                $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-                $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-                $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-                $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-
+                continue;
             }
-        }
 
-
-        public function create_user($usernameInput, $passInput, $fnameInput, $lnameInput, $emailInput){
-            global $connectstr_dbhost, $connectstr_dbname, $connectstr_dbpassword, $connectstr_dbusername;
-
-            if($link=mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname)) {
-                $sql = "INSERT INTO `users` (`username`,`password`, `fname`,`lname`, `email`) VALUES ('$usernameInput', '$passInput', '$fnameInput', '$lnameInput', '$emailInput')";
-                if (mysqli_query($link, $sql)) {
-                    if (($aff_rows = mysqli_affected_rows($link)) > 0) {
-                        echo "New record created successfully ($aff_rows)";
-                    } else {
-                        echo "Query Logic Error: $sql";
-                    }
-                } else {
-                    echo "Syntax Error:: ", mysqli_error($link);
-                }
-                mysqli_close($link);
-            }
-            else{
-                echo "Connection Error: ", mysqli_connect_error();
-            }
+            $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+            $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+            $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+            $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 
         }
-
-        public function login($usernameinput, $passwordinput){
-            global $connectstr_dbhost, $connectstr_dbname, $connectstr_dbpassword, $connectstr_dbusername;
-            if($link=mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname)) {
-                $sql = "SELECT * FROM `users` WHERE username = '.$usernameinput' and password = '.$passwordinput'";
-                if($result = mysqli_query($link, $sql)){
+    }
 
 
+    public function create_user($usernameInput, $passInput, $fnameInput, $lnameInput, $emailInput){
+        global $connectstr_dbhost, $connectstr_dbname, $connectstr_dbpassword, $connectstr_dbusername;
+
+        if($link=mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname)) {
+            $sql = "INSERT INTO `users` (`username`,`password`, `fname`,`lname`, `email`) VALUES ('$usernameInput', '$passInput', '$fnameInput', '$lnameInput', '$emailInput')";
+            if (mysqli_query($link, $sql)) {
                 if (($aff_rows = mysqli_affected_rows($link)) > 0) {
                     echo "New record created successfully ($aff_rows)";
                 } else {
@@ -68,37 +45,50 @@
             }
             mysqli_close($link);
         }
-    else{
-    echo "Connection Error: ", mysqli_connect_error();
-    }
-
-
-
-
-
-
-
-
-
-
-
-                $count = mysqli_num_rows($result);
-
-                return ($count == 1);
-
-
-            }
-
-
+        else{
+            echo "Connection Error: ", mysqli_connect_error();
         }
 
+    }
 
+    public function login($usernameinput, $passwordinput){
+        global $connectstr_dbhost, $connectstr_dbname, $connectstr_dbpassword, $connectstr_dbusername;
+        $count = "";
+        if($link=mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname)) {
+            $sql = "SELECT * FROM `users` WHERE username = '.$usernameinput' and password = '.$passwordinput'";
+            $result = mysqli_query($link, $sql);
+            $count = mysqli_num_rows($result);
+            if($result != 0){
+                if ($count == 1) {
+                    echo "New record created successfully";
+                } else {
+                    echo "Query Logic Error: $sql";
+                }
+            } else {
+                echo "Syntax Error:: ", mysqli_error($link);
+            }
+            mysqli_close($link);
+        }
+        else{
+            echo "Connection Error: ", mysqli_connect_error();
+        }
 
-
-
+        echo $count;
+        return ($count == 1);
 
 
     }
 
 
-    ?>
+}
+
+
+
+
+
+
+
+}
+
+
+?>
