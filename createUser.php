1220781254a3170ee3b0422cea68856680c1ca8c
Unknown <mhm.sherpa@gmail.com>
2017-04-25 20:48:09 -0500

    bug fixes with createuser

--- createUser.php
+++ createUser.php
@@ -19,7 +19,7 @@
             <input id="emailinput" class="createuserinput" placeholder="Email Address" name="emailinput">
             <input id="logininput" class="createuserinput" placeholder="Username" name="logininput">
             <input id="passinput" class="createuserinput" placeholder="Password" name="passinput" type="password" style="padding:10px 15px;">
-            <input id="passvalidate" class="createuserinput" Placeholder="Password" name="passvalidate" type="password">
+            <input id="passvalidate" class="createuserinput" Placeholder="Password" name="passvalidate" type="password" style="padding:10px 15px;">
         </div>
 
 
