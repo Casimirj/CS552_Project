8e2f6895d0bc04baa20c71b4cc5367ed981dea65
Unknown <mhm.sherpa@gmail.com>
2017-04-29 15:22:41 -0500

    createuser button on homenow functional

--- home.php
+++ home.php
@@ -7,7 +7,7 @@ include("header.php");
 <div class="buttonwrapper">
     <div class="homepagebutton"><div class="buttonimage material-icons">add_to_queue</div><div class="buttontext">Add Course</div></div>
     <div class="homepagebutton"><div class="buttonimage material-icons">remove_from_queue</div><div class="buttontext">Delete Course</div></div>
-    <div class="homepagebutton"><div class="buttonimage material-icons">add_circle</div><div class="buttontext">Add User</div></div>
+    <a class="homepagebutton" href="createUser.php"><div class="buttonimage material-icons">add_circle</div><div class="buttontext">Add User</div></a>
     <div class="homepagebutton"><div class="buttonimage material-icons">remove_circle</div><div class="buttontext">Delete User</div></div>
     <div class="homepagebutton"><div class="buttonimage material-icons">perm_identity</div><div class="buttontext">Payroll</div></div>
     <div class="homepagebutton"><div class="buttonimage material-icons">receipt</div><div class="buttontext">Billing</div></div>
