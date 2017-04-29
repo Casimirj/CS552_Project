2019022c1ec8e4958702588d34f016f873d39661
Unknown <mhm.sherpa@gmail.com>
2017-04-29 18:52:41 -0500

    styling displayusers page

--- displayUsers.php
+++ displayUsers.php
@@ -4,13 +4,61 @@ include("header.php");
 require("acmedb.php");
 
 
+?>
+
+
+<h1>Users</h1>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
 
 $database = new Acmedb();
-
 $database->connect();
+$result = $database->getUsers();
 
 
-$database->getUsers();
+echo ("
+<table border='1'>
+<tr>
+<th>ID</th>
+<th>First Name</th>
+<th>Last Name</th>
+<th>Username</th>
+<th>Email</th>
+</tr>
+
+");
+
+while($row = mysqli_fetch_array($result))
+{
+    echo(
+        "<tr>" .
+        "<td>" . $row['id']. "</td>".
+        "<td>" . $row['fname'] . "</td>".
+        "<td>" . $row['lname'] . "</td>".
+        "<td>" . $row['username'] . "</td>".
+        "<td>" . $row['email'] . "</td>".
+        "</tr>"
+
+    );
+}
+
+
+echo "</table>";
 
 
 
