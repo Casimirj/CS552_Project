c47b00c2f9cc2f7420c14989bf04c23e7b227385
Unknown <mhm.sherpa@gmail.com>
2017-04-29 19:30:59 -0500

    removed delete button from users table

--- displayUsers.php
+++ displayUsers.php
@@ -36,13 +36,8 @@ while($row = mysqli_fetch_array($result))
         "<td>" . $row['lname'] . "</td>".
         "<td>" . $row['username'] . "</td>".
         "<td>" . $row['email'] . "</td>".
-        "<td class='contact-delete'>
-            <form action='delete.php?name='"); echo $row['id']; echo"' method='post'>
-                <input type='hidden' name='name' value="; echo $row['id']; echo"
-                <input type='submit' name='submit' value='Delete'>
-            </form>
-        </td>";
-        echo"</tr>";
+        "</tr>"
+    );
 
 
 }
