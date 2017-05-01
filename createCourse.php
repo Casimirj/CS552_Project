20927723867a0ac1db0571d04a311ed3d1792074
Unknown <mhm.sherpa@gmail.com>
2017-05-01 18:25:32 -0500

    .

--- createCourse.php
+++ createCourse.php
@@ -8,6 +8,17 @@
         <?php
 
             if(isset($_GET['id'])){
+                $_POST['employeeID'] = $_GET['$id'];
+                echo"
+                <div class=\"createuserinputwrapper\">
+                    <input id=\"fnameinput\" class=\"createuserinput\" placeholder=\"First Name\" name=\"fnameinput\">
+                    <input id=\"lnameinput\" class=\"createuserinput\" placeholder=\"Last Name\" name=\"lnameinput\">
+                    <input id=\"emailinput\" class=\"createuserinput\" placeholder=\"Email Address\" name=\"emailinput\">
+                    <input id=\"logininput\" class=\"createuserinput\" placeholder=\"Username\" name=\"usernameinput\">
+                    <input id=\"passinput\" class=\"createuserinput\" placeholder=\"Password\" name=\"passinput\" type=\"password\">
+                    <input id=\"passvalidate\" class=\"createuserinput\" Placeholder=\"Password\" name=\"passvalidate\" type=\"password\">
+
+                </div>";
 
             }
             else{
@@ -18,21 +29,7 @@
 
 
         ?>
-        </div>
-        <div class="createuserinputwrapper">
-            <input id="fnameinput" class="createuserinput" placeholder="First Name" name="fnameinput">
-            <input id="lnameinput" class="createuserinput" placeholder="Last Name" name="lnameinput">
-            <input id="emailinput" class="createuserinput" placeholder="Email Address" name="emailinput">
-            <input id="logininput" class="createuserinput" placeholder="Username" name="usernameinput">
-            <input id="passinput" class="createuserinput" placeholder="Password" name="passinput" type="password">
-            <input id="passvalidate" class="createuserinput" Placeholder="Password" name="passvalidate" type="password">
 
-            <?php if($_SESSION['usertype'] == 1){
-                echo("
-                <input id='usertype' class=createuserinput placeholder='User Type' name=usertype>
-                ");
-            }?>
-        </div>
 
 
         <input id=createuserbutton class="acmebutton" type="submit" value="Create Account">
