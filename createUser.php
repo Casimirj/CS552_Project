983598d54311fa8cf03140bee76cca4c3238a477
Unknown <mhm.sherpa@gmail.com>
2017-05-01 21:07:02 -0500

    createuser button on loginpage now works even if the user isnt logged in

--- createUser.php
+++ createUser.php
@@ -1,5 +1,37 @@
-<?php include("header.php");?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+
+    <script src="script.js"></script>
+
+</head>
+<body>
+
+<div class="header">
+    <a href="home.php"><div class="col-sm-2 acmelogo"></div></a>
+    <div class="col-sm-7"></div>
+    <div class="col-sm-3 profile">
+        <div><?php echo $_SESSION['name'];?></div><div class="material-icons" style="margin-right:20px">face</div>
+    </div>
+</div>
+
+
+
+
+
+
+<div class="content">
 
 <div class="createaccounttoptext">Create an Account</div>
 <div class="createuserwrapper">
